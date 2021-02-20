b258b789b05cb15086bfc5bd4c018b33dd3bb8b9
dnsjdbstlr <dnsjdbstlr@naver.com>
2021-02-20 23:44:27 +0900

    .
    .

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from src.adv import adventure
 
 ### 기본설정 ###
 bot = commands.Bot(command_prefix='!')
-#token = 'NzgxNzgyNzQ5NDc5Njk4NDQy.X8Cp7A.wJ69VOJUvfEMnv6-F63QG8KNans'
-token = 'NzgyMTc4NTQ4MTg1NTYzMTQ3.X8Iaig.0o0wUqoz8j_iub3SC7A5SFY83U4'
+token = 'NzgxNzgyNzQ5NDc5Njk4NDQy.X8Cp7A.wJ69VOJUvfEMnv6-F63QG8KNans'
+#token = 'NzgyMTc4NTQ4MTg1NTYzMTQ3.X8Iaig.0o0wUqoz8j_iub3SC7A5SFY83U4'
 
 ### 이벤트 ###
 @bot.event
